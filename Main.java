7a1c8f8df577867855ce42d9f22a901490c68829
HARSH PANDYA <117256499+harsh852003@users.noreply.github.com>
2023-04-13 23:13:24 +0530

    remove comment

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 public class Main {
 
-    // create a method
+  
     public int addNumbers(int a, int b) {
       int sum = a + b;
-      // return value
       return sum;
     }
   
@@ -11,10 +10,8 @@ public class Main {
       
       int num1 = 25;
       int num2 = 15;
-  
-      // create an object of Main
+ 
       Main obj = new Main();
-      // calling method
       int result = obj.addNumbers(num1, num2);
       System.out.println("Sum is: " + result);
     }
