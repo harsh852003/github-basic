439e47f67568fcc983ad28259274bed74c4b2386
Bhaskar Pandya <pandyaharsh852003@gmail.com>
2023-04-16 22:45:45 +0530

    try and catch example

---
+++ try_catch.java
@@ -0,0 +1,13 @@
+class try_catch {
+    public static void main(String[] args) {
+  
+      try {
+        int divideByZero = 5 / 0;
+        System.out.println("Rest of code in try block");
+      }
+  
+      catch (ArithmeticException e) {
+        System.out.println("ArithmeticException => " + e.getMessage());
+      }
+    }
+  }
